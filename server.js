d85a0586c0fbdf81b559486e608ac155758b63d9
Ben <menubar@protonmail.com>
2018-10-28 01:51:28 -0400

    new

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+const Bundler = require('parcel-bundler')
+const bundler = new Bundler('src/index.html')
+const PORT = process.env.PORT || 3000
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({
+  apiKey: process.env.API_KEY
+})
+
+app.get('/api', async (req, res) => {
+  let data = await airtable
+    .base(process.env.BASE)
+    .table(process.env.TABLE)
+    .list()
+
+  res.json(data)
+})
+
+app.use(bundler.middleware())
+
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`)
+})
--- src/App.js
+++ src/App.js
@@ -1,68 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './slant.css';
-import 'whatwg-fetch';
-require('env2')('.env');
-
-// Add environmental vars to .env
-// Find them at: https://airtable.com/api
-// Base looks like appsDmF4srKVAXHqp
-// The default view is Main%20View (unless renamed)
-
-const config = {
-  base: process.env.AIRTABLE_BASE,
-  table: process.env.AIRTABLE_TABLE,
-  view: process.env.AIRTABLE_VIEW,
-  apiKey: process.env.AIRTABLE_API_KEY,
-  maxRecords: process.env.AIRTABLE_MAX_RECORDS
-}
-
-const request = new Request(`https://api.airtable.com/v0/${config.base}/${config.table}?maxRecords=${config.maxRecords}&view=${config.view}`, {
-  method: 'get',
-  headers: new Headers({
-    'Authorization': `Bearer ${config.apiKey}`
-  })
-});
+import React from 'react'
+import 'whatwg-fetch'
+import { render } from 'react-dom'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { records: [] };
-    this.fetchAirtable = this.fetchAirtable.bind(this);
+  state = {
+    records: []
   }
 
   async componentDidMount() {
-    await this.fetchAirtable()
-  }
-
-  async fetchAirtable() {
-    var resp = await fetch(request).catch(err => {console.log(err)})
-    if(resp.status >= 200 && resp.status < 300) {
-      var json = await resp.json()
-      const {records} = json;
-      this.setState({ records });
-    }
+    let resp = await fetch('/api', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    let json = await resp.json()
+    const { records } = json
+    this.setState({ records })
   }
 
   render() {
-    var {records} = this.state
+    let { records } = this.state
     return (
-
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-    
         <div>
         {records && records.length > 0 ? records.map(record =>
           <p>{JSON.stringify(record)}</p>
         ) : <p>Double-check that you have added your API key to .env.</p>}
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+render(
+  <App />,
+  document.getElementById('root')
+)
